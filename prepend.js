702d2d68baa93445b00e14dd15a4ca0d34155dda
Nikolay Rademacher <hrrdx33@gmail.com>
2021-10-09 02:34:11 +0200

    chore: cleanup

--- prepend.js
+++ prepend.js
@@ -4,12 +4,14 @@ import { close, openSync, readFileSync, writeSync } from 'fs';
 function prepend() {
   const text = process.argv[2];
   const file = process.argv[3];
+
   const data = readFileSync(file);
   const fd = openSync(file, 'w+');
   const buffer = Buffer.from(text);
 
   writeSync(fd, buffer, 0, buffer.length, 0);
   writeSync(fd, data, 0, data.length, buffer.length);
+
   close(fd);
 
   return 0;
