2c021d19b04f26f3c55bd53df6b5879ae694a2e0
Nikolay Rademacher <hrrdx33@gmail.com>
2021-10-12 17:04:12 +0200

    chore: update readme

--- file-ops.js
+++ file-ops.js
@@ -1,30 +1,52 @@
 #!/usr/bin/env node
 import glob from 'glob';
-import { findAndReplace, prepend } from './lib';
+import { readFileSync, openSync, writeSync, close } from 'fs';
+import replace from 'replace';
 
 const [, , opFlag, ...args] = process.argv;
 
-const [, operation] = opFlag;
+const [, operation] = opFlag.split("=");
 
 const target = args[args.length - 1];
+
+console.log(operation, target)
 
 glob(target, function (err, files) {
   if (err) {
     throw err;
   }
 
-  return files.forEach(function (file, _index, _array) {
+  return files.forEach(function (item, _index, _array) {
     switch (operation) {
       case 'fnr':
         const [regex, replacement] = args;
 
-        findAndReplace(file, regex, replacement);
+        console.log(item + ' found');
+        console.log(readFileSync(item, 'utf8'));
+
+        replace({
+          regex,
+          replacement,
+          paths: [item],
+          recursive: true,
+          silent: true,
+        });
+
+        console.log('Replacement complete');
+        console.log(readFileSync(item, 'utf8'));
 
         return 0;
       case 'prepend': {
         const [text] = args;
 
-        prepend(file, text);
+        const data = readFileSync(item);
+        const fd = openSync(item, 'w+');
+        const buffer = Buffer.from(text);
+
+        writeSync(fd, buffer, 0, buffer.length, 0);
+        writeSync(fd, data, 0, data.length, buffer.length);
+
+        close(fd);
 
         return 0;
       }
